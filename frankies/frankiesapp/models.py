11d20e84cf663c642ade5ca883193a30ec0d7194
Jansen Lao <jansen.lao@obf.ateneo.edu>
2022-02-28 16:54:35 +0800

    fix

--- frankies/frankiesapp/models.py
+++ frankies/frankiesapp/models.py
@@ -2,20 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model): #FIX THIS JANSEN CHANGE THE CHARFIELDS
-    product_id = models.CharField(max_length=300, primary_key=True)
+    product_id = models.CharField(max_length=300, primary_key=True, unique=True)
     #product_collection = models.ForeignKey(Product_Collection, on_delete=models.CASCADE) Wait for CJ's class or remove 
     product_name = models.CharField(max_length=300)
     product_description = models.TextField(max_length=300)
-    product_image = models.FileField(upload_to=None) #upload and display image
+    product_image = models.FileField(null=True, blank=True) #upload and display image
     product_materialsused = models.CharField(max_length=300) #collection of materials
-    product_quantity = models.IntegerField(max_length=10)
-    product_unitprice = models.IntegerField(max_length=10)
+    product_quantity = models.IntegerField()
+    product_unitprice = models.IntegerField()
     product_type = models.CharField(max_length=300) #list of copllections but can add
     product_visibility = models.BooleanField(max_length=300) #Visibility switch
-    product_colors = models.CharField(max_length=300) #Probably a list of colors
+    product_colors1 = models.CharField(max_length=300) #Probably a list of colors
+    product_colors2 = models.CharField(max_length=300)
+    product_colors3 = models.CharField(max_length=300)
+    product_colors4 = models.CharField(max_length=300)
+    product_colors5 = models.CharField(max_length=300)
+    product_colors6 = models.CharField(max_length=300)
+    product_colors7 = models.CharField(max_length=300)
+    product_colors8 = models.CharField(max_length=300)
+
 
     def __str__(self):#DONT KNOW IF THIS IS NEEDED
-        return f"{self.product_id}, {self.product_name}, {self.product_description}, {self.product_image}, {self.product_materialsused}, {self.product_quantity}, {self.product_unitprice}, {self.product_type}, {self.product_visibility}, {self.product_colors}"
+        return f"{self.product_id}, {self.product_name}, {self.product_description}, {self.product_image}, {self.product_materialsused}, {self.product_quantity}, {self.product_unitprice}, {self.product_type}, {self.product_visibility}, {self.product_colors1}, {self.product_colors2}, {self.product_colors3}, {self.product_colors4}, {self.product_colors5}, {self.product_colors6}, {self.product_colors7}, {self.product_colors8}, "
 
 class Material(models.Model): 
     material_id = models.CharField(max_length=300)
--- frankies/frankiesapp/views.py
+++ frankies/frankiesapp/views.py
@@ -6,7 +6,6 @@ def hello_world(request):
     return render(request, 'frankiesapp/hello_world.html')
 
 def add_product(request):
-    product_object = Product.objects.all()
     if(request.method=="POST"):
         product_id = request.POST.get('product_id')
         product_name = request.POST.get('product_name')
@@ -17,10 +16,18 @@ def add_product(request):
         product_unitprice = request.POST.get('product_unitprice')
         product_type = request.POST.get('product_type')
         product_visibility = request.POST.get('product_visibility')
-        product_colors = request.POST.get('product_colors')
+        product_colors1 = request.POST.get('product_colors1')
+        product_colors2 = request.POST.get('product_colors2')
+        product_colors3 = request.POST.get('product_colors3')
+        product_colors4 = request.POST.get('product_colors4')
+        product_colors5 = request.POST.get('product_colors5')
+        product_colors6 = request.POST.get('product_colors6')
+        product_colors7 = request.POST.get('product_colors7')
+        product_colors8 = request.POST.get('product_colors8')
 
         try:
-            Product.objects.create(product_name=product_name, 
+            Product.objects.create(product_id=product_id,
+            product_name=product_name, 
             product_description=product_description, 
             product_image=product_image,  
             product_materialsused= product_materialsused,  
@@ -28,7 +35,15 @@ def add_product(request):
             product_unitprice=product_unitprice, 
             product_type=product_type, 
             product_visibility=product_visibility, 
-            product_colors=product_colors)
+            product_colors1=product_colors1,
+            product_colors2=product_colors2,
+            product_colors3=product_colors3,
+            product_colors4=product_colors4,
+            product_colors5=product_colors5,
+            product_colors6=product_colors6,
+            product_colors7=product_colors7,
+            product_colors8=product_colors8)
+
 
             message = "Product created successfully"
             return render(request, 'frankiesapp/add_product.html', {'message':message})
